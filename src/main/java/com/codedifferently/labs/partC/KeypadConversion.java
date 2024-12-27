8c9d0e50a96e7adc62354945f1208c1629646507
tmoicoles5 <colestmoi@gmail.com>
2024-12-27 11:44:51 -0500

    code

--- src/main/java/com/codedifferently/labs/partC/KeypadConversion.java
+++ src/main/java/com/codedifferently/labs/partC/KeypadConversion.java
@@ -11,8 +11,36 @@ public class KeypadConversion {
 
         String message = myScan.nextLine().toLowerCase(); //Converting message into lowercase letters
 
-        System.out.println("Keypad sequence:"+convertToKeypad(message));
+        System.out.println("Keypad sequence:" + convertToKeypad(message));
 
-    myScan.close();
+        myScan.close();
     }
+
+    public static String convertToKeypad(String message) {
+        String[] keypad = {
+                "2", "22", "222",
+                "3", "33", "333",
+                "4", "44", "444",
+                "5", "55", "555",
+                "6", "66", "666",
+                "7", "77", "777", "7777",
+                "8", "88", "888",
+                "9", "99", "999"
+
+        }; //An array that implements numbers to letters
+            // Maps the array
+
+        StringBuilder result = new StringBuilder();// StringBuilder creates an object.
+
+        for(char c : message.toCharArray()){
+            if (c>= 'a'&& c<='z'){
+                result.append(keypad[c-'a']);
+            }
+        }
+
+        return result.toString();
+    }
+
+
+
 }
