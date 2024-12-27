a31aac4cf57b081f688f5450e4954705677d371e
tmoicoles5 <colestmoi@gmail.com>
2024-12-27 09:08:24 -0500

    changes

--- src/main/java/com/codedifferently/labs/partB/ex03/AbsoluteValue.java
+++ src/main/java/com/codedifferently/labs/partB/ex03/AbsoluteValue.java
@@ -3,5 +3,8 @@ package com.codedifferently.labs.partB.ex03;
 public class AbsoluteValue {
     public static void main(String[] args) {
         int a = - 445, b = 399, c = 33 - 212;
+        int totalSum = a + b +c;
+       int absoluteValue = Math.abs(totalSum);
+        System.out.println(absoluteValue);
     }
 }
--- src/main/java/com/codedifferently/labs/partC/KeypadConversion.java
+++ src/main/java/com/codedifferently/labs/partC/KeypadConversion.java
@@ -1,4 +1,18 @@
 package com.codedifferently.labs.partC;
 
+
+import java.util.Scanner;
+
 public class KeypadConversion {
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+
+        System.out.println("Enter you keypad numbers:");
+
+        String message = myScan.nextLine().toLowerCase(); //Converting message into lowercase letters
+
+        System.out.println("Keypad sequence:"+convertToKeypad(message));
+
+    myScan.close();
+    }
 }
